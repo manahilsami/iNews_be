4586a1daf7afb695d020d1dffe5a834c7696e08a
Manahil Sami <manahil_sami@outlook.com>
2025-09-18 07:40:08 -0400

    auth middleware added

---
+++ middlewares/auth.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../utils/config");
+const { UnauthorizedError } = require("../utils/customErrors");
+
+const auth = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith("Bearer ")) {
+    return next(new UnauthorizedError("Authorization token required"));
+  }
+
+  const token = authorization.replace("Bearer ", "");
+  let payload;
+
+  try {
+    payload = jwt.verify(token, JWT_SECRET);
+  } catch (err) {
+    return next(new UnauthorizedError("Invalid token"));
+  }
+
+  req.user = payload;
+  return next();
+};
+
+module.exports = auth;
